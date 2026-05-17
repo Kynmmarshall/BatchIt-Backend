9c2ee5cdf21029115577a310c935f6885d4a51e2
Kynmmarshall <kynmmarshall@gmail.com>
2026-05-17 03:29:08 +0100

    fix django to load .env values

--- batchit_proj/settings.py
+++ batchit_proj/settings.py
@@ -1,9 +1,13 @@
 import os
 from pathlib import Path
 import dj_database_url
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Load environment variables from project-level .env file.
+load_dotenv(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
