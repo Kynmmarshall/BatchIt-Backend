b996d1c3fa0b40441a7c7ef70d96e37246487e5b
Kynmmarshall <kynmmarshall@gmail.com>
2026-05-26 10:57:28 +0100

    update removal of legacy token

--- batchit_proj/settings.py
+++ batchit_proj/settings.py
@@ -116,7 +116,6 @@ CORS_ALLOW_CREDENTIALS = True
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.TokenAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
